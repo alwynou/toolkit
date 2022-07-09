0b72d082bd7daf675ef4c02ee690f5bdffeb843e
Aiden <108968414+aidenou@users.noreply.github.com>
2022-07-09 17:32:47 +0800

    feat(map): add func

---
+++ packages/map/index.ts
@@ -0,0 +1,23 @@
+type IterateFn<T> = (item: any, index: number | string, data: T) => any
+type IterateSyncFn<T> = (item: any, index: number | string, data: T) => Promise<any>
+
+export async function map<T extends Array<any>>(
+  data: T,
+  iterate: IterateFn<T> | IterateSyncFn<T>,
+  context?: any
+) {
+  const rets: any[] = []
+  let len = data.length
+  while (len) {
+    const index = len - 1
+    try {
+      // eslint-disable-next-line no-await-in-loop
+      const ret = await iterate.call(context, data[index], index, data)
+      rets.push(ret)
+    } catch (error) {
+      rets.push(null)
+    }
+    len--
+  }
+  return rets
+}
