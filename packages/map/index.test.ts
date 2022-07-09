11a26dab421c999a4a3af89506f159fff24ed443
Alwyn <alwynou@163.com>
2022-07-09 18:02:51 +0800

    fix(map): test desc

--- packages/map/index.test.ts
+++ packages/map/index.test.ts
@@ -1,8 +1,8 @@
 import { expect, describe, it } from 'vitest'
 import { map } from './index'
 
-describe('isPromise', () => {
-  it('should be truthy', async () => {
+describe('map', () => {
+  it('async map', async () => {
     expect(await map([1, 2, 3], (n) => Promise.resolve(n * 2))).toEqual([2, 4, 6])
   })
 })
