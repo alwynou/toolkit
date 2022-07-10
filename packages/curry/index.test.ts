818018faa295283e06e7aedeb8904a2824f0b16f
Alwyn <alwynou@163.com>
2022-07-10 15:59:56 +0800

    feat(curry): add curry func

---
+++ packages/curry/index.test.ts
@@ -0,0 +1,12 @@
+import { describe, expect, it } from 'vitest'
+import { curry } from './index'
+
+describe('curry', () => {
+  it('curry add', () => {
+    const add = curry((a:number, b:number, c:number) => a + b + c)
+    expect(add(1)(2)(3)).toBe(6)
+    expect(add(1, 2)(3)).toBe(6)
+    expect(add(1, 2, 3)).toBe(6)
+    expect(add(1, 2)).toBeTypeOf('function')
+  })
+})
