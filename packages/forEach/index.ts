049b16cce5b0f0331e2720eea9838d2f0d5a2253
AlwynOu <alwynou@163.com>
2022-08-17 17:07:05 +0800

    upd(forEach): add default context

--- packages/forEach/index.ts
+++ packages/forEach/index.ts
@@ -23,18 +23,20 @@ export function forEach<T extends ForEachData>(
   context?: any,
 ): void {
   if (data) {
+    const _context = context || data
+
     if (Array.isArray(data)) {
       let len = data.length
       while (len) {
         const index = len - 1
-        iterate.call(context, data[index], index, data)
+        iterate.call(_context, data[index], index, data)
         len--
       }
     }
     else if (isObject(data)) {
       for (const key in data) {
         if (Reflect.has(data, key))
-          iterate.call(context, data[key], key, data)
+          iterate.call(_context, data[key], key, data)
       }
     }
   }
