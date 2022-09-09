bfe1eeb337e7eb5f170e9ed621359acc227285ed
AlwynOu <alwynou@163.com>
2022-09-10 00:47:38 +0800

    feat(forEach): support string source data

--- packages/forEach/index.ts
+++ packages/forEach/index.ts
@@ -1,5 +1,6 @@
 import { isArray } from '../isArray'
 import { isObject } from '../isObject'
+import { isString } from '../isString'
 import { typeOf } from '../typeOf'
 /**
  * 遍历对象和数组
@@ -16,11 +17,12 @@ import { typeOf } from '../typeOf'
  * @param fn
  */
 export function forEach<T extends Array<any>>(source: T, fn: (value: T[number], index: number, source: T) => void): void
+export function forEach<T extends string>(source: T, fn: (value: T[number], index: number, source: T) => void): void
 export function forEach<T extends Record<string | symbol, any>>(source: T, fn: (value: T[keyof T], key: keyof T, source: T) => void, config?: { symbol?: boolean }): void
 export function forEach<T extends Map<any, any>>(source: T, fn: (value: T[keyof T], key: keyof T, source: T) => void): void
 export function forEach<T extends Set<any>>(source: T, fn: (value: T[keyof T], key: number, source: T) => void): void
 export function forEach<T>(source: T, fn: (value: any, index: any, source: T) => void, config?: { symbol?: boolean }) {
-  if (isArray(source)) {
+  if (isArray(source) || isString(source)) {
     const len = source.length
     for (let index = 0; index < len; index++)
       fn(source[index], index, source)
